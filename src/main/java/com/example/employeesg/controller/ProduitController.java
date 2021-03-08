9108edfc5d625ea79ecddb3d54449d094541235b
taher_chourabi <tchourabi@gmail.com>
2021-03-08 19:15:22 +0100

    crud

--- src/main/java/com/example/employeesg/controller/ProduitController.java
+++ src/main/java/com/example/employeesg/controller/ProduitController.java
@@ -1,13 +1,19 @@
 package com.example.employeesg.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.employeesg.entitys.Book;
+import com.example.employeesg.entitys.Employee;
 import com.example.employeesg.entitys.Produit;
+import com.example.employeesg.model.ProduitAjoutModel;
 import com.example.employeesg.repository.ProduitRepository;
 
 @RestController 
@@ -20,22 +26,41 @@ public class ProduitController {
 	@Autowired
 	private ProduitRepository produitRepository;
 	
-	@GetMapping("/add")
+	@GetMapping("/list")
 
-	public void ajouterProduit(){
-		System.out.println("raho jeni appel men and ll postman");
+	public List<Produit> getProduitsList(){
 		
-		/*Produit p = new Produit();
-		
-		p.setLabel("COCA");
-		p.setPrice(500);
-		
-		this.produitRepository.save(p);*/
-		
-		
-		
+		return this.produitRepository.findAll();
 	}
 		
+	@PostMapping("/add")
+	private void addNewProduit( @RequestBody ProduitAjoutModel p ) {
+		Produit produit = new Produit();
+		
+		produit.setLabel(p.getTitreProduit());
+		produit.setPrice(p.getPrixProduit());
+		
+		
+		this.produitRepository.save(produit);
+		  
+	}
+	
+	 @GetMapping("/delete/{id}")
+	 public void deleteProduit(@PathVariable(value ="id") Long id){
+		 this.produitRepository.delete(   this.produitRepository.findById(id).get()  );
+	 }
+	 
+	 @PostMapping("/update/{id}")
+	 public void modiferProduit(@PathVariable(value ="id") Long id,@RequestBody ProduitAjoutModel p ){
+		 
+		 Produit produit =  this.produitRepository.findById(id).get();
+		 
+		 produit.setPrice(p.getPrixProduit());
+		 produit.setLabel(p.getTitreProduit());
+		 
+		 this.produitRepository.save(produit);
+		 
+	 }
 	
 	
 	
---
+++ src/main/java/com/example/employeesg/model/ProduitAjoutModel.java
@@ -0,0 +1,24 @@
+package com.example.employeesg.model;
+
+public class ProduitAjoutModel {
+
+	private String titreProduit;
+	private int prixProduit;
+	
+	
+	public String getTitreProduit() {
+		return titreProduit;
+	}
+	public void setTitreProduit(String titreProduit) {
+		this.titreProduit = titreProduit;
+	}
+	public int getPrixProduit() {
+		return prixProduit;
+	}
+	public void setPrixProduit(int prixProduit) {
+		this.prixProduit = prixProduit;
+	}
+	
+	
+	
+}
